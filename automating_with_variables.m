0c3deb8c971a74f97abaf41a7805bf28badc3412
mohitk74 <163093956+mohitk74@users.noreply.github.com>
2024-06-12 15:04:14 +0530

    Update automating_with_variables.m

--- automating_with_variables.m
+++ automating_with_variables.m
@@ -24,34 +24,34 @@
 % A = timetable(time,valA);
 % B = timetable(time,valB);
 %--------------------------------------------------------------------------
-load('acd.mat')
 
+load('acd.mat');
 
 % Loading the simulink Model
-modelName='Sample_Model';
+modelName='Wiper_washer_final';
 open_system(modelName);
 
 % Creating the test harness having source as 'From Workspace' and Sink 'Outport'
-% sltest.harness.delete(modelName,'Sample_Model_Harnes7')
+% sltest.harness.delete(modelName,'Wiper_washer_final_Harnes7')
    
-sltest.harness.create(modelName, 'Name', 'Sample_Model_Harnes7', 'Source', 'From Workspace', 'Sink', 'Outport');
+% sltest.harness.create(modelName, 'Name', 'Wiper_washer_final_Harnes7', 'Source', 'From Workspace', 'Sink', 'Outport');
 
 % Opening the test Harness model
-sltest.harness.open(modelName,'Sample_Model_Harnes7');
+sltest.harness.open(modelName,'Wiper_washer_final_Harnes7');
 %--------------------------------------------------------------------------
 % test Manager
-tf = sltest.testmanager.TestFile('Sample_Model_Results1.mldatx');
+tf = sltest.testmanager.TestFile('Wiper_washer_final_Results1.mldatx');
 ts = getTestSuites(tf);
 tc = getTestCases(ts);
 
 % To provide the Sample Model & Test Harness Model to the Test Manager
 setProperty(tc,'Model',modelName)
-setProperty(tc, 'HarnessName', 'Sample_Model_Harnes7', 'HarnessOwner', modelName);
+setProperty(tc, 'HarnessName', 'Wiper_washer_final_Harnes7', 'HarnessOwner', modelName);
 
 % Capturing the Baseline
-baseline = captureBaselineCriteria(tc,'Sample_Model_Baseline.xlsx',true);
-sc = getSignalCriteria(baseline);
-sc(1).AbsTol = 9;
+% baseline = captureBaselineCriteria(tc,'Wiper_washer_final_Baseline.xlsx',true);
+% sc = getSignalCriteria(baseline);
+% sc(1).AbsTol = 9;
 
 % Opening the test Manager & Running the test cases
 sltest.testmanager.view;
@@ -59,7 +59,7 @@ sltest.testmanager.view;
 
 result = sltest.testmanager.run();
 
-exampleFile = 'Sample_Model_Results1.mldatx';
+exampleFile = 'Wiper_washer_final_Results1.mldatx';
 sltest.testmanager.load(exampleFile);
 %--------------------------------------------------------------------------
 % Executing the test manager
